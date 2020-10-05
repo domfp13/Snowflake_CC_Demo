62b5d82140a9f2f2eecbc08b242b7436e440addb
Fuentes <lf188653@compucom.com>
2020-10-05 10:13:37 -0500

    Adding 03_clustering.sql

--- 02_caching.sql
+++ 02_caching.sql
@@ -1,4 +1,5 @@
-/* Case 1: Understanding Cloud Services Layer cache*/
+/***** Case 1: Understanding Cloud Services Layer cache *****/
+
 -- Create temporary table 
 CREATE OR REPLACE TEMPORARY TABLE SUPPLIER AS
 SELECT * 
@@ -11,20 +12,19 @@ ALTER WAREHOUSE TALEND_XS SUSPEND;
 
 SELECT * FROM SUPPLIER;
 
-/* Case 2: Understanding VWH cache */
--- Disabling the cache and run after disabling the cache
+/***** Case 2: Understanding VWH cache *****/
+
+-- Disabling the cloud services layer cache
 ALTER SESSION SET USE_CACHED_RESULT = FALSE;
 SELECT * FROM SUPPLIER;
-
--- Rerun the query immediatly. Data will be fetched from virtual warehouses
+-- Rerun the query immediatly, data will be fetched from cache virtual warehouses
 SELECT * FROM SUPPLIER;
 
 ALTER WAREHOUSE TALEND_XS SUSPEND;
 
 SELECT * FROM SUPPLIER;
 
-
-/* Case 3: Changing query */
+/***** Case 3: Changing query *****/
 
 -- Suspend warehouse execute
 ALTER WAREHOUSE TALEND_XS SUSPEND;
@@ -32,7 +32,7 @@ SELECT * FROM SUPPLIER;
 -- Run but different query
 SELECT * FROM SUPPLIER LIMIT 1000;
 
-/* Case 4: What happens when we delete a record? */
+/***** Case 4: What happens when we delete a record? *****/
 
 ALTER SESSION SET USE_CACHED_RESULT = TRUE;
 
@@ -44,7 +44,7 @@ SELECT * FROM SUPPLIER;
 
 SELECT * FROM SUPPLIER;
 
-/* More info about VWH */
+/***** More info about VWH *****/
 SELECT * FROM TABLE(information_schema.warehouse_load_history(date_range_start=>dateadd('hour',-1,current_timestamp())));
 
 SELECT * FROM TABLE(information_schema.warehouse_metering_history(dateadd('sec',-500,current_date()),current_date()));
